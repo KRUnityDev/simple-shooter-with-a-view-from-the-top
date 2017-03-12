cf34963e38064fd85ab67e92887f7d35809c0a9d
zmienny <zmiennoimienny@gmail.com>
2017-03-12 14:14:48 +0100

    Add PlayerControl Class

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerControl : MonoBehaviour {
+
+    public float Speed = 10f;
+    Rigidbody2D playerRigidbody;
+
+    private void Awake()
+    {
+        playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+
+    #if UNITY_STANDALONE
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        playerRigidbody.velocity = new Vector2(horizontalAxis*Speed, verticalAxis*Speed);
+    #endif
+    #if UNITY_ANDROID
+        //TODO
+    #endif
+
+    }
+}
